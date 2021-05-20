91fa8982f96ea570616f870f25b1d7926280a719
Eric Leask <16efl@queensu.ca>
2021-05-20 08:59:21 -0400

    Line Detection Work
    Hough transforms(probablistic) implemented however not working completely as expected. Increasing the size of accumulators was expected to produce larger lines however areas of the image as still not being detected. Attempts are inprogress to improve the hough transform output.
    Algorithm produced to merge lines with a similar gradient and location in image

--- detect_shapes.py
+++ detect_shapes.py
@@ -10,7 +10,7 @@ import cv2 as cv
 import numpy as np
 from numpy import array
 import openpyscad as ops
-
+from scipy.spatial import distance
 import math
 import matplotlib.pyplot as plt
 
@@ -187,6 +187,8 @@ fa[1].append("open morph" + str(3))
 erosion = cv.erode(opening,cv.getStructuringElement(cv.MORPH_RECT,(3,3)),iterations = 1)
 fa[0].append(erosion)
 fa[1].append("erode morph")
+#erosion  = cv.morphologyEx(erosion, cv.MORPH_OPEN,  cv.getStructuringElement(cv.MORPH_RECT,(3,3)))
+
 for i in range(len(fa[0])):
     r=3
     #plt.subplot(int(np.ceil(len(fa[0])/r)),r,i+1),#plt.imshow(fa[0][i],'gray',vmin=0,vmax=255)
@@ -204,10 +206,10 @@ fa[1].append("gray")
 cdstP = cv.cvtColor(erosion, cv.COLOR_GRAY2BGR)
 #cv.imshow("Source", cdstP)
 j=19
-if j:
-#for j in range(7,25,3):
-    linesP = cv.HoughLinesP(erosion, j , np.pi / 180 , 50, None, 15, 10)
-    
+#if j:
+for j in range(1,52,10):
+    linesP = cv.HoughLinesP(image=erosion, rho=j , theta=(np.pi / (180)), threshold=50,minLineLength=2, maxLineGap=5)
+    cdstP = cv.cvtColor(erosion, cv.COLOR_GRAY2BGR)
     if linesP is not None:
         for i in range(0, len(linesP)):
             l = linesP[i][0]
@@ -241,32 +243,35 @@ if j:
     mi=np.flip(np.argsort(magnitudes)) #magnitude index from  largest to smallest 
     #group lines by direction of lines
     linesH=np.squeeze(linesP)
+    lineSet=[]
     #linesF[index,0=lines,1=center x,y , 2=direction x,y]
-    lli=mi[0]#largestLineIndex
-    groupLinesIndexes=[[lli]]
-    mi=np.delete(mi,0,0)
-    r_linesH=np.delete(linesH,lli,0)
-    r_centers=np.delete(centers,lli,0)
-    r_directions=np.delete(directions,lli,0)
-    
-    i = len(r_linesH)-1
-    #lets go through all the lines
+    while len(mi)>0:
+        lli=mi[0]#largestLineIndex
+        groupLinesIndexes=[lli]
+        mi=np.delete(mi,0,0)
+        #r_linesH=np.delete(linesH,lli,0)
+        #r_centers=np.delete(centers,lli,0)
+        #r_directions=np.delete(directions,lli,0)
+        r_linesH=linesH
+        r_centers=centers
+        r_directions=directions
+        l = lli
+        #lets go through all the lines
+        i = len(r_linesH)-1
+        for i in range(len(r_linesH)-1,-1,-1):
+        #while i>0:
+            line=r_linesH[i]
+            center=r_centers[i]
+            direction=r_directions[i]
+            
 
-    while len(r_linesH)>0:
-        line=r_linesH[i]
-        center=r_centers[i]
-        direction=r_directions[i]
-        r_linesH=np.delete(r_linesH,i,0)
-        r_centers=np.delete(r_centers,i,0)
-        r_directions=np.delete(r_directions,i,0)
-        
-        for l in groupLinesIndexes:
+            
             #lets compare the angle between the lines
-
             x=np.dot(direction,np.squeeze(directions[l]))
             y=np.dot(np.hypot(direction[0],direction[1]), np.hypot(directions[l,0],directions[l,1]))
-            x=x/y
-            ang=np.arccos(x)*180/np.pi
+            z=x/y
+            ang=np.squeeze(np.arccos(z)*180/np.pi)
+
             if ang>90:
                 ang=abs(180-ang)
             print("ang: " + str(ang))
@@ -280,13 +285,45 @@ if j:
                 mag=np.hypot(directions[l,0],directions[l,1])
                 d=eq_top/mag #this isn't reliable because as the point is farther away this line could be incorrect
                 if d < 20: #This is classfied as a potential match
-                    groupLinesIndex              
-        i=i-1    
+                    groupLinesIndexes.append(i)
+                    #r_linesH=np.delete(r_linesH,i,0)
+                    #r_centers=np.delete(r_centers,i,0)
+                    #r_directions=np.delete(r_directions,i,0)
+                    mi=mi[mi != i] 
             
+            #Now that i have the group of lines I need to make the largest line             
+            i=i-1    
+        lineGroup=[r_linesH[index] for index in groupLinesIndexes]
+        cdstP = cv.cvtColor(erosion, cv.COLOR_GRAY2BGR)
+        for i in range(0, len(lineGroup)):
+            li = lineGroup[i]
+            cv.line(cdstP, (li[0], li[1]), (li[2], li[3]), (0,0,255), 2 , cv.LINE_AA)
+            plt.imshow(cdstP)
+        plt.show()
+        coordGroup=np.reshape(lineGroup,(-1, 2))
+        maxCoords=[coordGroup[0],coordGroup[1]]
+        coordGroup=np.delete(coordGroup,[0,1],0)
+        for i in range(len(coordGroup)-1,-1,-1):
+            #consider points a,b,c
+            a=maxCoords[0]
+            b=maxCoords[1]
+            c=coordGroup[i]
+            max_sol=np.argmax([distance.sqeuclidean(a,b),distance.sqeuclidean(a,c),distance.sqeuclidean(b,c)])
+            #if a<->b is max then we update nothing 
+            if max_sol==1: #a<->c is max so replace b with c in maxCoords
+                maxCoords[1]=coordGroup[i]
+            elif max_sol==2:#b<->c is max so replace a with c in maxCoords
+                maxCoords[0]=coordGroup[i]
+        lineSet.append(maxCoords)
+        cdstP = cv.cvtColor(erosion, cv.COLOR_GRAY2BGR)
+        cv.line(cdstP, (maxCoords[0][0], maxCoords[0][1]), (maxCoords[1][0], maxCoords[1][1]), (0,255,0), 2 , cv.LINE_AA)
+        plt.imshow(cdstP)
+        plt.show()
+                
             
             
 
-
+print(lineSet)
 exit()
 #======================================================================================
 #Create CAD
